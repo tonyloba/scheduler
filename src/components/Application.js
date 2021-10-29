ce51f3275438bd0aa65100adcfbb90e20fecbd4a
tonyloba <tonyloba@gmail.com>
2021-10-29 02:19:10 -0400

    updated application with appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,7 +7,21 @@ import Appointment from "./Appointment/index";
 
 export default function Application(props) {
   const [day,setDay] = useState('Monday');
-  console.log(day)
+  // console.log(day)
+
+  const schedule = appointments.map((appointment) => {
+    
+    
+    return (
+      <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={appointment.interview}
+
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -32,7 +46,8 @@ export default function Application(props) {
       </section>
 
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+          {schedule}       
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
@@ -43,20 +58,60 @@ export default function Application(props) {
 
 
 
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
