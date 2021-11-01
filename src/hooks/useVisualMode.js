c3adf5e7ca9a6ea39aad8d8c884a031cb9e48c48
tonyloba <tonyloba@gmail.com>
2021-10-31 21:40:18 -0400

    updated modes

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -29,37 +29,3 @@ export default function useVisualMode(initial) {
 
 
 
-
-
-
-
-
-// const useVisualMode = (FIRST) => {
-//   const [mode, setMode] = useState(FIRST);
-//   const [history, setHistory] = useState([FIRST]);
-
-//   const transition = (SECOND, replace=false) => {
-//     setMode(SECOND);
-//     if(replace && history.length !== 1){
-//       setHistory(prev => ([...prev.slice(0, prev.length - 1), SECOND]))
-//    } else {
-//      setHistory(prev => ([...prev, SECOND]));
-//    }
-
-//   };
-
-//   const back = () => {
-//     if (history.length !== 1) {
-//       history.pop();
-//       setMode(history[history.length - 1])
-//     }
-//   };
-
-//   return { 
-//     mode,
-//     transition,
-//     back
-//   };
-// };
-
-// export { useVisualMode }
