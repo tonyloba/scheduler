5b1f67b20101ab9ec30de49653fec5982897537c
Anton Lobarev <74120447+tonyloba@users.noreply.github.com>
2022-01-01 21:23:34 -0500

    Update navigation.spec.js

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,11 +2,16 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
-});
 
-it("should navigate to Tuesday", () => {
-  cy.visit("/");
-  cy.contains("[data-testid=day]", "Tuesday").click()
-  .should('have.class', "day-list__item--selected");
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    // cy.contains("day-list__item day-list__item--selected", "Tuesday").click()
+    // .should("have.css", "day-list__item--selected");
 
+  });
+
+  it("should highlight Tuesday when selected", () => {
+
+    cy.contains("li", "Tuesday").should("have.css", "background-color", "rgba(0, 0, 0, 0)");
+  });
 });
